1c5594c2dea6ff2a9224f21452a4036d8953f8d1
kyj1206 <139724986+kyj1206@users.noreply.github.com>
2024-01-09 17:21:48 +0900

    thymeleaf로 블로그 화면 구현 마무리

--- src/main/java/blogproject/blogproject/controller/BlogViewController.java
+++ src/main/java/blogproject/blogproject/controller/BlogViewController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -30,10 +31,23 @@ public class BlogViewController {
     }
 
     @GetMapping("/articles/{id}")
-    public String getArticle(@PathVariable long id, Model model) {
+    public String getArticle(@PathVariable Long id, Model model) {
         Article article = blogService.findById(id);
         model.addAttribute("article", new ArticleViewResponse(article));
 
         return "article";
+    }
+
+    @GetMapping("/new-article")
+    public String newArticle(@RequestParam(required = false) Long id, Model model) {
+
+        if(id == null) {
+            model.addAttribute("article", new ArticleViewResponse()); // id 값이 없다면 생성
+        } else { // id 값이 있다면 수정
+            Article article = blogService.findById(id);
+            model.addAttribute("article", new ArticleViewResponse(article));
+        }
+
+        return "newArticle";
     }
 }
